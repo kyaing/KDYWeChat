7bd5c29424d5c6846eed7f76f425bc061dd62202
kaideyi <kaideyi@haodai.com>
2016-12-06 16:47:05 +0800

    update

---
+++ KDYWeChat/BizClass/Base/BaseViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  BaseViewModel.swift
+//  KDYWeChat
+//
+//  Created by mac on 16/12/6.
+//  Copyright © 2016年 kaideyi.com. All rights reserved.
+//
+
+import Foundation
+
+class BaseViewModel {
+    
+}
+
